7cb05634794096aeda18647dc0e4b03603c808de
ttoonoott <rimb9pkmink@gmail.com>
2018-05-17 08:00:42 +0700

    skill component

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+export class ProgressBar extends Component {
+  state = {}
+  render() {
+    const Move = keyframes`
+      0% {
+        width: 0%
+      }
+      100% {
+        width: ${this.props.percent}%
+      }
+    `;
+    const ProgressBarBg = styled.div`
+      background-color: #777;
+      border-radius: 8px;
+      padding: 2px;
+      width: 100%;
+      position: relative;
+    `;
+    const Progress = styled.div`
+      background-color: white;
+      height: 5px;
+      border-radius: 5px;
+      animation: 3s ${Move} normal forwards ease-in-out;
+    `;
+    const Name = styled.p`
+      font-size: 12px;
+      margin: 0;
+    `;
+    const Rate = styled.p`
+      font-size: 12px;
+      margin: 0px;
+      text-align: right;
+    `;
+    return (
+      <div style={{ width: this.props.width }} >
+        <Name style={{ fontSize: 12, marginBottom: 0 }} >{this.props.name}</Name>
+        <ProgressBarBg className="progress-bar" >
+          <Progress />
+        </ProgressBarBg>
+        <Rate style={{ marginTop: 2, fontSize: 10 }} >{this.props.percent / 10}/10</Rate>
+      </div>
+    );
+  }
+}
+
+export default ProgressBar;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProgressBar from './ProgressBar';
 
 export class Skill extends Component { // eslint-disable-line
   state = {
@@ -7,6 +8,15 @@ export class Skill extends Component { // eslint-disable-line
   render() {
     return (
       <div>
+        <ProgressBar width="350px" percent={50} name="HTML" />
+        <ProgressBar width="350px" percent={60} name="Javascipt" />
+        <ProgressBar width="350px" percent={50} name="Css" />
+        <ProgressBar width="350px" percent={60} name="ReactJS" />
+        <ProgressBar width="350px" percent={40} name="React-Native" />
+        <ProgressBar width="350px" percent={45} name="NodeJS" />
+        <ProgressBar width="350px" percent={35} name="Mongo" />
+        <ProgressBar width="350px" percent={65} name="Self-learning" />
+        <ProgressBar width="350px" percent={80} name="Comnunicate with team" />
       </div>
     );
   }
